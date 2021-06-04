cc77b5023a9b93ff26b3dc31f4b7bf9844a2511c
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-06-04 10:07:26 -0500

    loop practice

---
+++ js/loop_classwork.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// alert("this works")
+var message = '';
+//var howMany=prompt("how many");
+
+for (let index = 0; index < 10; index++) {
+    message = message + index + "\n";
+
+}
+
+alert(message);
+
+alert(index);
