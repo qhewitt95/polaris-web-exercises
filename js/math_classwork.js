22952d39edde42ce7f1498e4d56ab7dd03a1f9da
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-06-09 12:31:44 -0500

    halfway

---
+++ js/math_classwork.js
@@ -0,0 +1,23 @@
+// Returns a random integer between min and max.
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(getRandomInt(1,2)); //coin toss
+console.log(getRandomInt(1,52)); //card
+console.log(getRandomInt(1,6)); //dice
+
+console.log(Math.round(23.5436)); //round
+
+console.log(Math.floor(23.43667)); //interger smaller then arguement
+console.log(Math.floor(23));
+
+console.log(Math.ceil(23.3526)) // intrger larger then argument
+console.log(Math.pow(3, 3)) // raise to a power
+
+console.log(Math.sqrt(25)) // square root
+
+console.log(Math.E); //natural algorithims
+
+console.log(Math.PI); //3.14 pi
+
