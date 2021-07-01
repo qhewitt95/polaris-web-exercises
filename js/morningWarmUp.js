6b1ba51b299ff57046d7e14fa946d748fe33110e
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-07-01 09:30:44 -0500

    new js

---
+++ js/morningWarmUp.js
@@ -0,0 +1,18 @@
+// TODO: Create a function named countSpaces that accepts a string and returns the total number of spaces in the string.
+function countSpaces(input) {
+    if(typeof input === "string") {
+        let input = "These are my words";
+        let spaceCount = (myWords.split(" ").length-1)
+        console.log(spaceCount)
+        return spaceCount;
+    } else {
+        console.log("ERROR")
+        return false;
+    }
+}
+var result = countSpaces("These are words")
+console.log(result)
+
+
+
+// TODO: Add validation to function above, if the argument pass is not a string it should return false.
