9e64c0dd5171c90d27208593a25217190968e033
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-07-09 14:29:11 -0500

    wip

---
+++ js/jquery-style.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+
+    var h1 = $("h1")
+    h1 = $("h1")
+
+    console.log(h1)
+    console.log(h1.html("The champ is here "))
+
+    alert("Watch this!")
+
+    var h2= $('h2')
+    h2 = $('h2')
+
+    console.log(h1)
+    console.log(h2.html("This is actually kinda wack #rip"))
+
+
+    $('#biglist').css('background-color', 'yellow')
+    $('#samelist').css('background-color', 'brown')
+
+    $('#discordghost').css(border)
+
+}) 
