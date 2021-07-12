1073820639a80f4bec30e037fc128a2396a8fb5a
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-07-12 12:24:37 -0500

    finished ex

--- js/jquery-style.js
+++ js/jquery-style.js
@@ -14,7 +14,20 @@ $(document).ready(function (){
     console.log(h1)
     console.log(h2.html("This is actually kinda wack #rip"))
 
-
+    $( "#bigheader" ).click(function() {
+        $(this).css('background-color', 'red');
+    });
+    $( "#ptagss" ).dblclick(function() {
+        $(this).css('font-size', '40px');
+    });
+    $('li').hover(
+        function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+            $(this).css('color', 'black');
+        }
+    );
     $('#biglist').css({backgroundColor: "#ffe", borderLeft: "10px solid #ccc" })
 
     $('#samelist').css('background-color', 'green', 'border:')
@@ -23,6 +36,17 @@ $(document).ready(function (){
     $('.codeup').css('border', '1px red solid').css({backgroundColor: "#ffe", borderTop: "10px solid black"});
 
     $('li').css('font-size', '20px')
+
+
+    $('#main-header').hover(
+        function() {
+            $(this).css('background-color', 'red');
+        },
+        function() {
+            $(this).css('background-color', 'green');
+        }
+    );
+
     /*$('h1').css('background-color', 'yellow');
     $('p').css('background-color', 'yellow');
     $('li').css('background-color', 'yellow');*/
