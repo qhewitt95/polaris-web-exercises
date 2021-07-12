aa49c19898b5d7644e054a9582d304c8d203bfa0
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-07-12 10:48:05 -0500

    done

--- js/jquery-style.js
+++ js/jquery-style.js
@@ -15,9 +15,15 @@ $(document).ready(function (){
     console.log(h2.html("This is actually kinda wack #rip"))
 
 
-    $('#biglist').css('background-color', 'yellow')
-    $('#samelist').css('background-color', 'brown')
+    $('#biglist').css({backgroundColor: "#ffe", borderLeft: "10px solid #ccc" })
 
-    $('#discordghost').css(border)
+    $('#samelist').css('background-color', 'green', 'border:')
+    $('#pTag').css({backgroundColor: "#ddd", borderRight: "10px solid #bbb"})
 
+    $('.codeup').css('border', '1px red solid').css({backgroundColor: "#ffe", borderTop: "10px solid black"});
+
+    $('li').css('font-size', '20px')
+    /*$('h1').css('background-color', 'yellow');
+    $('p').css('background-color', 'yellow');
+    $('li').css('background-color', 'yellow');*/
 }) 
